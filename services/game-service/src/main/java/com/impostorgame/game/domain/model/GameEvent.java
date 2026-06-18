2fc508637390a94c6ae607c1b4d73e56c109095d
perecollet <pcollet@telefonica.net>
2026-06-18 13:54:00 +0200

    feat(game): add GamePhase and GameEvent enums

---
+++ services/game-service/src/main/java/com/impostorgame/game/domain/model/GameEvent.java
@@ -0,0 +1,5 @@
+package com.impostorgame.game.domain.model;
+
+public enum GameEvent {
+    GAME_STARTED, WORDS_DEALT, TIMER_EXPIRED, VOTE_COMPLETE, RESET
+}
---
+++ services/game-service/src/main/java/com/impostorgame/game/domain/model/GamePhase.java
@@ -0,0 +1,5 @@
+package com.impostorgame.game.domain.model;
+
+public enum GamePhase {
+    LOBBY, WORD_ASSIGNMENT, DISCUSSION, VOTING, RESULTS
+}
